a72ff18707e7b26bd24584ee8b85c4abc8cf690d
Justin Perumal <justinp@bbd.co.za>
2022-10-06 14:13:41 +0200

    Game Done

---
+++ Escape Game Jam Project/Assets/Scripts/SceneController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Pre-Level 1");
+    }
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public void LoadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
--- Escape Game Jam Project/Assets/Scripts/TargetController.cs
+++ Escape Game Jam Project/Assets/Scripts/TargetController.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TargetController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Vector2 movePos;
     public float moveSpeed; 
+    private Scene CurrentScene;
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 1f;
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         movePos = new Vector2(Random.Range(-12,12), Random.Range(-7,7));
+
+        CurrentScene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
@@ -32,9 +36,23 @@ public class TargetController : MonoBehaviour
         {
             Debug.Log("Click target");
             Destroy(this.gameObject);
+            if(CurrentScene.name == "Level 1")
+            {
+                SceneManager.LoadScene("Pre-Level 2");
+            }
+
+            if(CurrentScene.name == "Level 2")
+            {
+                SceneManager.LoadScene("Pre-Level 3");
+            }
+
+            if(CurrentScene.name == "Level 3")
+            {
+                SceneManager.LoadScene("End Game");
+            }
         }    
         else{
-            //GAME OVER
+            SceneManager.LoadScene("Game Over");
         }
     }
 }
